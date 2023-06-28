507785cee772a062e0c6ee947fc18e2c7acf269b
Yuki Kakui <yu-kakui@kddi-agile.com>
2023-06-28 11:17:40 +0900

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_gQ0j372p4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '26fdt6taas8akid5f8bdbss4m0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
