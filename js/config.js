45e46d3d2e4d7322e590f37d124ccb1a482e6d0e
Yuki Kakui <yu-kakui@kddi-agile.com>
2023-06-28 15:51:44 +0900

    new configuration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://3dqt02vcbg.execute-api.ap-northeast-1.amazonaws.com/yu-kakui_prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
